bfbeb4a05dfe95b0f888132bfd485f90d40e0101
krishnapatel29 <70604936+krishnapatel29@users.noreply.github.com>
2021-04-15 16:53:08 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+# Read complex binary
+# open filename and return the contents as a column vector treating
+# them as 32 bit complex numbers
+
+import numpy as np
+
+
+filename = r"C:\Users\patel\PycharmProjects\read_complex_binary\print_test2.dat"
+content = np.fromfile(open(filename), dtype=np.float32)
+for x in content:
+    a_file = open("test.txt", "w+")
+    print(content, a_file)
+    np.savetxt(a_file, content)
+
